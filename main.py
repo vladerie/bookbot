3d61bd133c2bbb26e56e6fb7c73db3ee0a069660
Vladerie <valerie-veldhuizen@live.nl>
2024-12-25 10:45:21 -0400

    added the get text and get number functions

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    book_path="books/frankenstein.txt"
+    text=get_book_text(book_path)
+    num_words= get_num_words(text)
+    print(num_words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+main()
