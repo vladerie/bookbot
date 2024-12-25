1c8bc90c9452807a31ce9a78938b72bd21f70301
Vladerie <valerie-veldhuizen@live.nl>
2024-12-25 11:02:05 -0400

    added character counter function and overall end report

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     book_path="books/frankenstein.txt"
     text=get_book_text(book_path)
     num_words= get_num_words(text)
-    print(num_words)
+    character_count=get_characters(text)
+    print(f"Analyzing {book_path}")
+    print("")
+    print(f"{num_words} words are present within the text")
+    for char in character_count:
+        print(f"{char} occurs {character_count[char]} times")
+    print("end of report")
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +17,16 @@ def get_book_text(path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_characters(text):
+    alphabet_dict = {chr(letter): 0 for letter in range(ord('a'), ord('z') + 1)}
+    lctext=text.lower()
+    for char in lctext:
+        if char in alphabet_dict:
+            alphabet_dict[char] += 1
+    return alphabet_dict
+
+
+
 
 main()
